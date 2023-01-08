7d1db02a83455fe9b9df4223a44f100c93d98de3
Prasad Dananjaya Wilagama <85976286+PrasadWilagama@users.noreply.github.com>
2023-01-08 23:20:07 +0530

    Add files via upload
    All files added to the Repo

---
+++ Water Bill Calculator/Water Bill Calculator/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace Water_Bill_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.txt_customerName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_prevReading = new System.Windows.Forms.TextBox();
+            this.txt_currentReading = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_calculate = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dgv_breakdown = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_costForUnits = new System.Windows.Forms.TextBox();
+            this.txt_fixedCost = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_totalCost = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_breakdown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txt_currentReading);
+            this.groupBox1.Controls.Add(this.txt_prevReading);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txt_customerName);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(22, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(804, 222);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Custormer Details";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Month";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "MM-yyyy";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(10, 36);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(85, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // txt_customerName
+            // 
+            this.txt_customerName.Location = new System.Drawing.Point(124, 36);
+            this.txt_customerName.Name = "txt_customerName";
+            this.txt_customerName.Size = new System.Drawing.Size(100, 20);
+            this.txt_customerName.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(121, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Customer Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Previous Reading";
+            // 
+            // txt_prevReading
+            // 
+            this.txt_prevReading.Location = new System.Drawing.Point(124, 76);
+            this.txt_prevReading.Name = "txt_prevReading";
+            this.txt_prevReading.Size = new System.Drawing.Size(100, 20);
+            this.txt_prevReading.TabIndex = 5;
+            // 
+            // txt_currentReading
+            // 
+            this.txt_currentReading.Location = new System.Drawing.Point(124, 113);
+            this.txt_currentReading.Name = "txt_currentReading";
+            this.txt_currentReading.Size = new System.Drawing.Size(100, 20);
+            this.txt_currentReading.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Current Reading";
+            // 
+            // btn_calculate
+            // 
+            this.btn_calculate.Location = new System.Drawing.Point(421, 259);
+            this.btn_calculate.Name = "btn_calculate";
+            this.btn_calculate.Size = new System.Drawing.Size(75, 23);
+            this.btn_calculate.TabIndex = 1;
+            this.btn_calculate.Text = "Calculate";
+            this.btn_calculate.UseVisualStyleBackColor = true;
+            this.btn_calculate.Click += new System.EventHandler(this.btn_calculate_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dgv_breakdown);
+            this.panel1.Location = new System.Drawing.Point(245, 20);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(474, 148);
+            this.panel1.TabIndex = 8;
+            // 
+            // dgv_breakdown
+            // 
+            this.dgv_breakdown.AllowUserToAddRows = false;
+            this.dgv_breakdown.AllowUserToDeleteRows = false;
+            this.dgv_breakdown.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_breakdown.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dgv_breakdown.Location = new System.Drawing.Point(3, 0);
+            this.dgv_breakdown.Name = "dgv_breakdown";
+            this.dgv_breakdown.ReadOnly = true;
+            this.dgv_breakdown.Size = new System.Drawing.Size(539, 220);
+            this.dgv_breakdown.TabIndex = 0;
+            this.dgv_breakdown.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Unit Range";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Cost Per Unit ";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Units ";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Total";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(642, 259);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Costs For Units";
+            // 
+            // txt_costForUnits
+            // 
+            this.txt_costForUnits.Location = new System.Drawing.Point(726, 256);
+            this.txt_costForUnits.Name = "txt_costForUnits";
+            this.txt_costForUnits.Size = new System.Drawing.Size(100, 20);
+            this.txt_costForUnits.TabIndex = 3;
+            // 
+            // txt_fixedCost
+            // 
+            this.txt_fixedCost.Location = new System.Drawing.Point(726, 293);
+            this.txt_fixedCost.Name = "txt_fixedCost";
+            this.txt_fixedCost.Size = new System.Drawing.Size(100, 20);
+            this.txt_fixedCost.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(664, 300);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Fixed Cost";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(665, 336);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Total Cost";
+            // 
+            // txt_totalCost
+            // 
+            this.txt_totalCost.Location = new System.Drawing.Point(726, 333);
+            this.txt_totalCost.Name = "txt_totalCost";
+            this.txt_totalCost.Size = new System.Drawing.Size(100, 20);
+            this.txt_totalCost.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 409);
+            this.Controls.Add(this.txt_totalCost);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_fixedCost);
+            this.Controls.Add(this.txt_costForUnits);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btn_calculate);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Water Bill Calculator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_breakdown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_currentReading;
+        private System.Windows.Forms.TextBox txt_prevReading;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_customerName;
+        private System.Windows.Forms.Button btn_calculate;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dgv_breakdown;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_costForUnits;
+        private System.Windows.Forms.TextBox txt_fixedCost;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_totalCost;
+    }
+}
+
---
+++ Water Bill Calculator/Water Bill Calculator/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Water_Bill_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_calculate_Click(object sender, EventArgs e)
+        {
+            Boolean errors = false;
+            int prevReading, currReading;
+
+            prevReading = 0;
+            currReading = 0;
+
+            //Custormer Name field
+            if (this.txt_customerName.Text == "")
+            { 
+             errors = true;
+
+            }
+
+            //previous Reading field
+            if (this.txt_prevReading.Text == "" ||
+                !Int32.TryParse(this.txt_prevReading.Text, out prevReading)) {
+                errors = true;
+            }
+
+            //current reading field 
+            if (this.txt_currentReading.Text == "" ||
+                !Int32.TryParse(this.txt_currentReading.Text, out currReading)) {
+                errors = true;
+            }
+
+            if (currReading < prevReading) { 
+            errors = true;
+            }
+            if (!errors)
+            {
+                //Calculation
+                int units = currReading - prevReading;
+
+                //Fixed Charge Cal 
+                int fixedCharge = 0;
+
+                if (units > 75)
+                {
+                    fixedCharge = 1600;
+
+                }
+
+                else if (units >= 51)
+                {
+                    fixedCharge = 1000;
+                }
+
+                else if (units >= 41)
+                {
+                    fixedCharge = 650;
+                }
+
+                else if (units >= 31)
+                {
+                    fixedCharge = 400;
+                }
+
+                else if (units >= 26)
+                {
+                    fixedCharge = 200;
+
+                }
+                else if (units >= 21)
+                {
+                    fixedCharge = 100;
+                }
+                else if (units >= 16)
+                {
+                    fixedCharge = 80;
+                }
+                else if (units >= 11)
+                {
+                    fixedCharge = 70;
+                }
+                else if (units >= 6)
+                {
+                    fixedCharge = 65;
+                }
+                else {
+                    fixedCharge = 50;
+                }
+
+                int remainingUnits = units;
+
+                int total = 0;
+
+                //Range 0-5
+                if (remainingUnits > 5)
+                {
+                    total += (5 * 12);
+                    remainingUnits -= 5;
+                    this.dgv_breakdown.Rows.Add("0-5", "Rs.12.00", 5, "Rs." + (5 * 12).ToString());
+                }
+                else {
+                    total += (remainingUnits * 12);
+                    this.dgv_breakdown.Rows.Add("0-5", "Rs.12.00", remainingUnits, "Rs." + (remainingUnits * 12).ToString());
+                    remainingUnits = 0;
+                    
+                }
+
+                //Range 6-10
+                if (remainingUnits > 5)
+                {
+                    total += (5 * 16);
+                    remainingUnits -= 5;
+                    this.dgv_breakdown.Rows.Add("6-10", "Rs.16.00", 5, "Rs." + (5 * 16).ToString());
+                }
+                else
+                {
+                    total += (remainingUnits * 16);
+                    this.dgv_breakdown.Rows.Add("6-10", "Rs.16.00", remainingUnits, "Rs." + (remainingUnits * 16).ToString());
+                    remainingUnits = 0;
+                }
+
+                //Range 11-15
+                if (remainingUnits > 5)
+                {
+                    total += (5 * 20);
+                    this.dgv_breakdown.Rows.Add("11-15", "Rs.20.00", 5, "Rs." + (5 * 20).ToString());
+                    remainingUnits -= 5;
+                   
+                }
+                else
+                {
+                    total += (remainingUnits * 20);
+                    this.dgv_breakdown.Rows.Add("11-15", "Rs.20.00", remainingUnits, "Rs." + (remainingUnits * 20).ToString());
+
+                    remainingUnits = 0;
+                }
+
+                //Range 16-20
+                if (remainingUnits > 5)
+                {
+                    total += (5 * 40);
+                    this.dgv_breakdown.Rows.Add("16-20", "Rs.40.00", 5, "Rs." + (5 * 40).ToString());
+                    remainingUnits -= 5;
+                    
+                }
+                else
+                {
+                    total += (remainingUnits * 40);
+                    this.dgv_breakdown.Rows.Add("16-20", "Rs.40.00", remainingUnits, "Rs." + (remainingUnits * 40).ToString());
+
+                    remainingUnits = 0;
+                }
+                //Range 21-25
+                if (remainingUnits > 5)
+                {
+                    total += (5 * 58);
+                    this.dgv_breakdown.Rows.Add("21-25", "Rs.58.00", 5, "Rs." + (5 * 58).ToString());
+                    remainingUnits -= 5;
+                    
+                }
+                else
+                {
+                    total += (remainingUnits * 58);
+                    this.dgv_breakdown.Rows.Add("21-25", "Rs.58.00", remainingUnits, "Rs." + (remainingUnits * 58).ToString());
+
+                    remainingUnits = 0;
+                }
+                //Range 26-30
+                if (remainingUnits > 5)
+                {
+                    total += (5 * 88);
+                    this.dgv_breakdown.Rows.Add("26-30", "Rs.88.00", 5, "Rs." + (5 * 88).ToString());
+                    remainingUnits -= 5;
+                    
+                }
+                else
+                {
+                    total += (remainingUnits * 88);
+                    this.dgv_breakdown.Rows.Add("26-30", "Rs.88.00", remainingUnits, "Rs." + (remainingUnits * 88).ToString());
+
+                    remainingUnits = 0;
+                }
+
+                //Range 31-40
+                if (remainingUnits > 10)
+                {
+                    total += (10 * 105);
+                    this.dgv_breakdown.Rows.Add("31-40", "Rs.105.00", 5, "Rs." + (5 * 105).ToString());
+                    remainingUnits -= 10;
+                    
+                }
+                else
+                {
+                    total += (remainingUnits * 105);
+                    this.dgv_breakdown.Rows.Add("31-40", "Rs.105.00", remainingUnits, "Rs." + (remainingUnits * 105).ToString());
+
+                    remainingUnits = 0;
+                }
+                //Range 41-50
+                if (remainingUnits > 10)
+                {
+                    total += (10 * 120);
+                    this.dgv_breakdown.Rows.Add("41-50", "Rs.120.00", 5, "Rs." + (5 * 120).ToString());
+                    remainingUnits -= 10;
+                    
+                }
+                else
+                {
+                    total += (remainingUnits * 120);
+                    this.dgv_breakdown.Rows.Add("41-50", "Rs.120.00", remainingUnits, "Rs." + (remainingUnits * 120).ToString());
+
+                    remainingUnits = 0;
+                }
+                //Range 51-75
+                if (remainingUnits > 25)
+                {
+                    total += (25 * 130);
+                    this.dgv_breakdown.Rows.Add("51-75", "Rs.130.00", 5, "Rs." + (5 * 130).ToString());
+                    remainingUnits -= 25;
+                   
+                }
+                else
+                {
+                    total += (remainingUnits * 130);
+                    this.dgv_breakdown.Rows.Add("51-75", "Rs.130.00", remainingUnits, "Rs." + (remainingUnits * 130).ToString());
+
+                    remainingUnits = 0;
+                }
+
+
+                // Range >75
+                if (remainingUnits > 0)
+                {
+                    total += (remainingUnits * 140);
+                    this.dgv_breakdown.Rows.Add("Over 75", "Rs.140.00", 5, "Rs." + (remainingUnits * 140).ToString());
+                }
+
+
+                this.txt_costForUnits.Text = total.ToString();
+                this.txt_fixedCost.Text = fixedCharge.ToString();
+                this.txt_totalCost.Text = (total+fixedCharge).ToString();
+
+
+
+
+
+
+
+            }
+            else {
+                MessageBox.Show("Errors are in the program !");
+            }
+
+             
+             
+            
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
